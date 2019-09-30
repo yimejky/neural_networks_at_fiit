89529fae4319a874b75bf132a1759bc26b101712
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-09-30 12:56:27 +0200

    quickfix

--- week_2/backstage/plots.py
+++ week_2/backstage/plots.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from week_2.backstage.load_data import load_data
-from week_2.model import LinearRegressionModel
+from solutions.week_2.model import LinearRegressionModel
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -267,7 +267,8 @@ def stochastic_plot():
         if ax_id == len(data.y):
             ax_xs, ax_ys = data.x, data.y
         else:
-            ax_xs, ax_ys = xs[ax_id, :], [ys[ax_id]]
+            ax_xs, ax_ys = np.array([xs[ax_id, :]]), [ys[ax_id]]
+        print(ax_xs)
 
         w, b = model.w, model.b
         for i, j in itertools.product(range(Z.shape[0]), range(Z.shape[1])):
@@ -290,7 +291,7 @@ def stochastic_plot():
             if ax_id == len(data.y):
                 ax_xs, ax_ys = data.x, data.y
             else:
-                ax_xs, ax_ys = xs[ax_id, :], [ys[ax_id]]
+                ax_xs, ax_ys = np.array([xs[ax_id, :]]), [ys[ax_id]]
 
             model.w, model.b = [w1], b
             dw, db = model.gradient(ax_xs, ax_ys)
