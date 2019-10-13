59cd4737a2af59982185da26e63ab1210b80e7c7
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-10-13 23:29:01 +0200

    W4 finishing touches

---
+++ week_4/mlp.py
@@ -0,0 +1,4 @@
+"""
+Copy your implementation of MultilayerPerceptron class from your notebook here for submission.
+Also append training related commands, such as model.compile, model.fit, etc.
+"""
