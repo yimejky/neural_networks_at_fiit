6a12488718a1f795883e4ab7a17926a286867593
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-10-21 00:54:04 +0200

    W5 done

--- week_5/mlp.py
+++ week_5/mlp.py
@@ -9,57 +9,57 @@ from tensorboard.plugins.hparams import api as hp
 
 class MultilayerPerceptron(keras.Model):
 
-    def __init__(self, dim_output, dim_hidden):
+    def __init__(self, dim_output, dim_hidden, activation, output_activation):
         super(MultilayerPerceptron, self).__init__(name='multilayer_perceptron')
         self.dim_output = dim_output
         self.dim_hidden = dim_hidden
 
         self.layer_1 = keras.layers.Dense(
-            units=dim_hidden)
+            units=dim_hidden,
+            activation=activation)
         self.layer_2 = keras.layers.Dense(
-            units=dim_output)
+            units=dim_output,
+            activation=output_activation)
 
     def call(self, x):
         h = self.layer_1(x)
         y = self.layer_2(h)
         return y
 
-# g_hparams = {
-#     'dim_hidden': hp.HParam('dim_hidden', hp.IntInterval(1, 1000)),
-#     'loss_function': hp.HParam('loss_function', hp.Discrete(['mse', 'categorial_crossentropy'])),
-#     'learning_rate': hp.HParam('learning_rate', hp.RealInterval(1e-10, 1.))
-# }
 
-# with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
-#   hp.hparams_config(
-#     hparams=g_hparams.values(),
-#     metrics=metrics,
-#   )
-
-
-def train(x, y, **hparams):
+def train(x, y, dim_output, **hparams):
 
     model = MultilayerPerceptron(
-        dim_output=hparams['dim_output'],
-        dim_hidden=hparams['dim_hidden'])
+        dim_output=dim_output,
+        dim_hidden=hparams['dim_hidden'],
+        activation=hparams['activation'],
+        output_activation=hparams['output_activation'])
 
     model.compile(
         optimizer=keras.optimizers.SGD(learning_rate=hparams['learning_rate']),
         loss=hparams['loss_function'],
         metrics=['accuracy'])
 
-    tensorboard_callback = keras.callbacks.TensorBoard(
-        log_dir=os.path.join("logs", timestamp()),
-        histogram_freq=1)
-    hparams_callback = hp.KerasCallback(
-        os.path.join("logs", timestamp()),
-        hparams),
-
-    model.fit(
+    hst = model.fit(
         x=x,
         y=y,
         batch_size=hparams['batch_size'],
         epochs=hparams['epoch'],
         validation_split=0.2,
-        callbacks=[tensorboard_callback, hparams_callback],
+        callbacks=[
+            keras.callbacks.TensorBoard(
+                log_dir=os.path.join("logs", timestamp()),
+                histogram_freq=1)],
         verbose=0)
+
+    best_validation_accuracy = max(hst.history['val_accuracy'])
+    final_loss = hst.history["loss"][-1]
+
+    # TensorBoard HParams saving
+    with tf.summary.create_file_writer(os.path.join('logs', timestamp(), 'hparams')).as_default():
+        hp.hparams(hparams)
+        tf.summary.scalar('hparams_accuracy', best_validation_accuracy, step=0)
+
+    return best_validation_accuracy, final_loss
+
+
